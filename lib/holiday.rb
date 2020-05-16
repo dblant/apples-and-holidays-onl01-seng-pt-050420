9542d1474fa72408df457a99b701fef85833447e
Dustin Blanton <dustinblntn@gmail.com>
2020-05-16 21:19:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,20 +70,14 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  collection = []
-  
-  holiday_hash.each do |a|
-    
-       collection << a
-         
-         
-       
-       
-     end
-    # collection.join(" ").chomp
-     collection.chomp
-     binding.pry
-  
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+  end
+end
+end
+
     
   
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -94,7 +88,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
